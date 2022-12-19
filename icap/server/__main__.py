7da278eceae3a32493209a062cbcda47e57de3a0
Diego Pereira <diego.pereira@nathanjames.com>
2022-12-19 00:03:09 -0300

    feature: icap functional (dummy)

--- icap/server/__main__.py
+++ icap/server/__main__.py
@@ -2,23 +2,29 @@
 # -*- coding: utf8 -*-
 
 import random
-import socketserver
+import SocketServer
 
 from pyicap import *
 
-class ThreadingSimpleServer(socketserver.ThreadingMixIn, ICAPServer):
+class ThreadingSimpleServer(SocketServer.ThreadingMixIn, ICAPServer):
     pass
 
 class ICAPHandler(BaseICAPRequestHandler):
 
-    def echo_OPTIONS(self):
+    def request_OPTIONS(self):
         self.set_icap_response(200)
         self.set_icap_header('Methods', 'RESPMOD')
         self.set_icap_header('Preview', '0')
         self.send_headers(False)
+        print('Got OPTIONS call')
 
-    def echo_RESPMOD(self):
+    def request_RESPMOD(self):
         self.no_adaptation_required()
+        print('Got RESPMOD call')
+
+    def request_REQMOD(self):
+        self.no_adaptation_required()
+        print('Got REQMOD call')
 
 port = 13440
 
--- squid/bin/entrypoint.sh
+++ squid/bin/entrypoint.sh
@@ -4,6 +4,8 @@ LOGFILE=/squid/var/logs/extended.log
 touch $LOGFILE
 chmod a+w $LOGFILE
 
+sleep 10  # wait for icap server to come online....
+
 /squid/sbin/squid
 
 tail -f $LOGFILE
