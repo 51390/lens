f9912c665a63fb49b8422b2f5acf680effb8ea6c
Diego Pereira <diegop@pm.me>
2023-02-02 21:16:09 -0300

    chore: sending chunks to send method in analyzer.

--- squid/analyzer/src/lib.rs
+++ squid/analyzer/src/lib.rs
@@ -1,8 +1,13 @@
-use std::fs::File;
+use std::fs::{
+    File,
+    OpenOptions
+};
+use std::ffi::c_void;
 use std::io::Write;
 
 #[no_mangle]
-pub extern "C" fn send() {
-    let mut file = File::create("/tmp/analyzer.txt");
-    file.expect("Unable to open file").write_all(b"We're alive!\n").ok();
+pub extern "C" fn send(buffer: c_void, size: usize) {
+    let mut file = OpenOptions::new().create(true).write(true).append(true).open("/tmp/analyzer.txt");
+    let mut msg = format!("Got a chunk with {} bytes.\n", size);
+    file.expect("Unable to open file").write_all(msg.as_bytes()).ok();
 }
