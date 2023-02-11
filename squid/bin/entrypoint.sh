423043a97bfdbaa7658391fb1e8c64a451aa42a5
Diego Pereira <diegop@pm.me>
2023-02-10 23:12:44 -0300

    chore: add support for running w/ valgrind instrumentation

--- squid/bin/entrypoint.sh
+++ squid/bin/entrypoint.sh
@@ -1,4 +1,5 @@
 
+ulimit -n 65536
 LOGFILE=/squid/var/logs/extended.log
 
 touch $LOGFILE
@@ -6,6 +7,19 @@ chmod a+w $LOGFILE
 
 sleep 10  # wait for icap server to come online....
 
-/squid/sbin/squid
+if [ $VALGRIND == 1 ]
+then
+    valgrind -v \
+        --trace-children=yes \
+        --num-callers=50 \
+        --log-file=/tmp/valgrind-%p.log \
+        --leak-check=full \
+        --leak-resolution=high \
+        --show-reachable=yes \
+        /squid/sbin/squid
+else
+    /squid/sbin/squid
+fi;
+
 
 tail -f $LOGFILE
