1425cf11cce14920112b0b43c6690909f2acfb97
Diego Pereira <diegop@pm.me>
2023-02-12 22:47:24 -0300

    feat: pass on content encoding header to analyzer

--- squid/analyzer/src/lib.rs
+++ squid/analyzer/src/lib.rs
@@ -1,6 +1,10 @@
 use std::fs::OpenOptions;
 use std::collections::HashMap;
-use std::ffi::c_void;
+use std::ffi::{
+    c_void,
+    c_char,
+    CStr
+};
 use std::io::Write;
 use std::sync::Once;
 use std::vec::Vec;
@@ -34,10 +38,10 @@ fn get_buffers() -> &'static mut Buffers {
     }
 }
 
-#[no_mangle]
-pub extern "C" fn append(id: i64, chunk: *const c_void, size: usize) {
-    let buffers = get_buffers();
+
+fn append(id: i64, chunk: *const c_void, size: usize) -> usize {
     let ptr = chunk as *const u8;
+    let buffers = get_buffers();
     let buffer_size;
     match buffers.transactions.get_mut(&id) {
         Some(buffer) => unsafe {
@@ -52,7 +56,12 @@ pub extern "C" fn append(id: i64, chunk: *const c_void, size: usize) {
             drop(buffers.transactions.insert(id, buffer));
         },
     }
+    buffer_size
+}
 
+#[no_mangle]
+pub extern "C" fn transfer(id: i64, chunk: *const c_void, size: usize) {
+    let buffer_size = append(id, chunk, size);
     let filename = format!("/tmp/request-body-{}.log", id);
     let file = OpenOptions::new().create(true).write(true).append(true).open(filename);
     let content = format!("Got a chunk with size {}. Buffers has a total of {} bytes.\n", size, buffer_size);
@@ -60,11 +69,16 @@ pub extern "C" fn append(id: i64, chunk: *const c_void, size: usize) {
 }
 
 #[no_mangle]
-pub extern "C" fn send(id: i64) {
+pub extern "C" fn commit(id: i64, content_encoding: *const c_char) {
     let buffers = get_buffers();
+    let buffer_size = match buffers.transactions.get_mut(&id) {
+        Some(buffer) => unsafe { buffer.len() },
+        None => 0
+    };
     buffers.transactions.remove(&id);
     let filename = format!("/tmp/request-body-{}.log", id);
     let file = OpenOptions::new().create(true).write(true).append(true).open(filename);
-    let content = "Finished appending, content transfer complete.\n";
+    let encoding = unsafe {CStr::from_ptr(content_encoding)}.to_str().unwrap();
+    let content = format!("Finished appending, content transfer complete. {} total bytes in buffer.\nContent encoding is {}.\n", buffer_size, encoding);
     file.expect("Unable to open file.").write_all(content.as_bytes()).ok();
 }
